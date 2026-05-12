d8e3879b9ee555076714ba51ac61f29835d7df06
joselopez02-cyber <jose.lopez02@unicatolica.edu.co>
2026-05-11 22:43:23 -0500

    feat: add domain entities (Pelicula, Sala, Funcion, Reserva, Usuario...)

--- src/main/java/cinema/monarca/MonarcaApplication.java
+++ src/main/java/cinema/monarca/MonarcaApplication.java
@@ -1,13 +1,10 @@
 package cinema.monarca;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MonarcaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MonarcaApplication.class, args);
-	}
-
+public class CinemaMonarcaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaMonarcaApplication.class, args);
+    }
 }
---
+++ src/main/java/cinema/monarca/model/Gestor.java
@@ -0,0 +1,28 @@
+package cinema.monarca.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "gestor")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Gestor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "manager_id")
+    private Long managerId;
+
+    @Column(name = "manager_details")
+    private String managerDetails;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sucursal_id")
+    private Sucursal sucursal;
+}
